538ef3cc6f616cedff56180dae9e0f743b5de85f
Schindler Róbert <schindler.robert_2020c@bolyai-szakkozep.hu>
2024-04-18 09:38:06 +0200

    Add project files.

---
+++ SR_EredmenyCLI/Eredmeny.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR_EredmenyCLI
+{
+    class Eredmeny
+    {
+        public int Id { get; set; }
+        public string Vezeteknev { get; set; }
+        public string Keresztnev { get; set; }
+        public string Targy { get; set; }
+        public int Szazalek { get; set; }
+        public int Erdemjegy
+        {
+            get
+            {
+                if (Szazalek < 20)
+                {
+                    return 1;
+                }
+                else if (Szazalek >= 20 && Szazalek < 40)
+                {
+                    return 2;
+                }
+                else if (Szazalek >= 40 && Szazalek < 60)
+                {
+                    return 3;
+                }
+                else if (Szazalek >= 60 && Szazalek < 80)
+                {
+                    return 4;
+                }
+                else
+                {
+                    return 5;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"Eredmény<{Vezeteknev} {Keresztnev}: {Erdemjegy}>";
+        }
+
+        public static Eredmeny CreateFromLine(string line, char separator = ',')
+        {
+            string[] values = line.Split(separator);
+
+            return new Eredmeny()
+            {
+                Id = int.Parse(values[0]),
+                Vezeteknev = values[1],
+                Keresztnev = values[2],
+                Targy = values[3],
+                Szazalek = int.Parse(values[4])
+            };
+        }
+
+    }
+}
---
+++ SR_EredmenyCLI/EredmenyRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR_EredmenyCLI
+{
+    class EredmenyRepository
+    {
+        public static string Path { get; set; }
+        public static bool SkipHeader { get; set; } = true;
+        public static char Separator { get; set; } = ',';
+
+        public static List<Eredmeny> FindAll()
+        {
+            using (StreamReader reader = new StreamReader(Path))
+            {
+                if (SkipHeader)
+                {
+                    reader.ReadLine();
+                }
+
+                List<Eredmeny> eredmeny = new List<Eredmeny>();
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    Eredmeny eredmenyline = Eredmeny.CreateFromLine(line, Separator);
+                    eredmeny.Add(eredmenyline);
+                }
+
+                return eredmeny;
+            }
+        }
+    }
+}
